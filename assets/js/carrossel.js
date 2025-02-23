045c7b3c672a3b67757e915fd6934e129b159462
Tewdric <manoelvictor756@gmail.com>
2025-02-23 17:27:50 -0300

    Finalizado

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -17,3 +17,46 @@ let currentIndex = 0;
         }
         
         setInterval(nextSlide, 2000); // Troca automática a cada 3 segundos
+
+const imgs = [
+            '../../img/aparmegiana-batata.jpeg',
+            '../../img/aparmegiana-panela.jpeg',
+            '../../img/assado-maionese.jpeg',
+            '../../img/carreteiro-ovo.jpeg',
+            '../../img/feijoada.jpeg',
+            '../../img/frango-aparmegiana.jpeg',
+            '../../img/frango-cane-panela.jpeg',
+            '../../img/frango-carne-molho.jpeg',
+            '../../img/frango-panqueca.jpeg',
+            '../../img/frango-rancombole.jpeg',
+            '../../img/guisado-bobo.jpeg',
+            '../../img/guisado.jpeg',
+            '../../img/lasanha-aparmegiana.jpeg'
+        ];
+
+const gallery = document.getElementById('gallery');
+
+        imgs.forEach(src => {
+            const img = document.createElement('img');
+            img.src = src;
+            img.alt = 'Imagem da galeria';
+            img.className = 'gallery-item';
+            img.onclick = () => {
+                
+                const modal = document.createElement('dialog');
+                const imgModal = document.createElement('img');
+                const closeButton = document.createElement('button');
+                closeButton.textContent = 'X';
+                modal.appendChild(closeButton);
+
+                imgModal.src = src;
+                imgModal.alt = 'Imagem do modal';
+                modal.appendChild(imgModal);
+                document.body.appendChild(modal);
+                modal.showModal();
+                closeButton.onclick = () => {
+                    modal.close();
+                };
+            }
+            gallery.appendChild(img);
+        });
